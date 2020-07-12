52031bff3f0ddecf1db4777dd7fb3ac5aae0d5ae
TomasKubin <Kubin.Tom@email.cz>
2020-07-12 19:28:53 +0200

    Slides

---
+++ todo-list/src/app/core/models/task-slide.enum.ts
@@ -0,0 +1,4 @@
+export enum TaskSlideEnum {
+    Daily = 0,
+    Monthly = 1
+}
--- todo-list/src/app/tab1/tab1.page.ts
+++ todo-list/src/app/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { TaskSlideEnum } from '../core/models/task-slide.enum';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
 
-  constructor() {}
+  @ViewChild(IonSlides) slides: IonSlides;
 
+  currentView = TaskSlideEnum.Daily;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    console.log(this.slides);
+  }
+
+
+  slideChanged() {
+    const currentIndexPromise = this.slides.getActiveIndex();
+
+    currentIndexPromise.then(currentIndex => {
+      this.currentView = currentIndex;
+    });
+  }
+
+  onCurrentViewChange(event) {
+    this.currentView = (Number(event.detail.value));
+
+    this.slides.slideTo(this.currentView, 400);
+  }
 }
